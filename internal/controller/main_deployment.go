adbf6cf0f930e77ce5c627beaf7e9f63611eb516
takuteh <takumi.ktym@gmail.com>
2026-09-24 13:18:28 +0000

    mainDeploymentの作成部分を分離

---
+++ internal/controller/main_deployment.go
@@ -0,0 +1,103 @@
+package controller
+
+import (
+	"context"
+
+	autolockv1alpha1 "github.com/takuteh/autolock-operator/api/v1alpha1"
+
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+func (r *AutolockReconciler) reconcileMainDeployment(
+	ctx context.Context,
+	autolock *autolockv1alpha1.Autolock,
+) error {
+	privileged := true
+	deployment := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "autolock-main",
+			Namespace: autolock.Namespace,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: &autolock.Spec.Main.Replicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": "autolock-main",
+				},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app": "autolock-main",
+					},
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  "autolock",
+							Image: autolock.Spec.Main.Image,
+							SecurityContext: &corev1.SecurityContext{
+								Privileged: &privileged,
+							},
+							VolumeMounts: []corev1.VolumeMount{
+								{
+									Name:      "autolock-config",
+									MountPath: "/home/pi/autolock/gear_version/etc/autolock_setting.json",
+									SubPath:   "autolock_setting.json",
+								},
+							},
+						},
+					},
+					Volumes: []corev1.Volume{
+						{
+							Name: "autolock-config",
+							VolumeSource: corev1.VolumeSource{
+								ConfigMap: &corev1.ConfigMapVolumeSource{
+									LocalObjectReference: corev1.LocalObjectReference{
+										Name: "autolock-config",
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	//クラスタからdeploymentを取得
+	var existingDeployment appsv1.Deployment
+	err := r.Get(
+		ctx,
+		types.NamespacedName{
+			Name:      "autolock-main",
+			Namespace: autolock.Namespace,
+		},
+		&existingDeployment,
+	)
+	//存在しなければ作成
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			if err = r.Create(ctx, deployment); err != nil {
+				return err
+			}
+
+			return nil
+		}
+
+		return err
+	}
+
+	// 存在する → 更新
+	existingDeployment.Spec = deployment.Spec
+
+	if err := r.Update(ctx, &existingDeployment); err != nil {
+		return err
+	}
+
+	return nil
+}
