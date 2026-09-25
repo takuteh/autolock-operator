a0e3e172b399ba6d1ea462783a0e7b85ec9369af
takuteh <takumi.ktym@gmail.com>
2026-09-25 03:16:10 +0000

    serviceのRBAC追加

--- internal/controller/autolock_controller.go
+++ internal/controller/autolock_controller.go
@@ -40,6 +40,7 @@ type AutolockReconciler struct {
 // +kubebuilder:rbac:groups=autolock.takuteh.uk,resources=autolocks/finalizers,verbs=update
 // +kubebuilder:rbac:groups="",resources=configmaps,verbs=get;create;update
 // +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;create;update
+// +kubebuilder:rbac:groups="",resources=services,verbs=get;create;update
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
